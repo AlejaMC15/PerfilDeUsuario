cf28c2345c0edead39e520bf9abc404835584376
MacBook Pro <usuario@MacBooks-MacBook-Pro.local>
2020-11-25 21:13:34 -0500

    second commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
 import React from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import Login from './components/Login';
+import Home from './components/Home';
 
 function App() {
   return (
     <div className="App">
-      <Login />
+      <Router>
+        <Switch>
+          <Route path="/info">
+            <Home />
+          </Route>
+          <Route path="/home">
+            <Home />
+          </Route>
+          <Route path="/">
+            <Login />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -1,42 +1,58 @@
 import React from 'react';
+import './style.css';
+import { useHistory } from 'react-router-dom';
+
 import Cat from '../../images/cat.jpg';
 
 const Login = () => {
+  let history = useHistory();
+
+  const goToHome = () => {
+    history.push('/home');
+    // handleClose();
+  };
+
   return (
-    <>
-      <div className="container-fluid">
-        <div className="row">
+    <div className="container container">
+      <div className="row align-items-center justify-content-center content">
+        <div className="col-sm-12 col-md-8 col-lg-6">
           <div className="col">
-            <div className="row">
-              <div className="col">
-                <h1>PROYECT NAME</h1>
-              </div>
-            </div>
-            <div className="row">
-              <div className="col">
-                <h2>DEVELOP BY: ALEJANDRA MENESES CARMONA</h2>
-              </div>
+            <h1 className="">PET PROFILE</h1>
+          </div>
+          <div className="row justify-content-center"></div>
+          <div className="row justify-content-center">
+            <div className="col text">
+              <h4 className="">DEVELOP BY:</h4>
+              <h2>ALEJANDRA MENESES CARMONA</h2>
             </div>
           </div>
-          <div className="col">
-            <div className="card" style={{ width: '18rem' }}>
-              <img src={Cat} className="imgRedonda" alt="img_profile" />
-              <div className="card-body">
-                <p>Username</p>
-                <input type="text" />
-              </div>
-              <div className="card-body">
-                <p>Password</p>
-                <input type="text" />
-              </div>
-              <button type="button" class="btn btn-primary">
-                Primary
+        </div>
+        <div className="col-sm-12 col-md-8 col-lg-6 d-flex justify-content-center">
+          <div className="card" style={{ width: '20rem' }}>
+            <div className="d-flex justify-content-center">
+              <img
+                src={Cat}
+                className="imgRedonda img-fluid"
+                alt="img_profile"
+              />
+            </div>
+            <div>
+              <p>Username</p>
+              <input className="input" type="text" />
+            </div>
+            <div>
+              <p>Password</p>
+              <input className="input" type="password" />
+            </div>
+            <div className="d-flex justify-content-center">
+              <button onClick={goToHome} type="button" class="btn btn-primary">
+                Login
               </button>
             </div>
           </div>
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
